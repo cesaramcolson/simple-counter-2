7c6e8d055ef5a5def630b7d197b35a96ae31a422
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2025-01-10 14:37:55 +0000

    Styles done to the simple counter

---
+++ src/js/component/SimpleCounter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SimpleCounter = () => {
+    return (<div className="counter">
+        <div className="icon"><i class="fa-regular fa-clock"></i></div>
+        <div className="five">0</div>
+        <div className="four">0</div>
+        <div className="three">0</div>
+        <div className="two">0</div>
+        <div className="one">0</div>
+    </div>);
+};
+
+export default SimpleCounter;
